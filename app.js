4caff0f7b713b17d4fc7dc30e4f10910484e104c
Andrew Singleton <andrewts@met.no>
2023-11-23 14:30:52 +0100

    Add stacked prob for precip and small refactor

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import { plume } from "./plume.js"
+import { stackedProb } from "./stackedProb.js"
 
 // Set up parameter information
 const getParamInfo = (param) => {
@@ -9,39 +10,47 @@ const getParamInfo = (param) => {
             units: "&#8451",
             colours: [d3.interpolateBlues(0.8), d3.interpolateBlues(0.5), d3.interpolateBlues(0.2)],
             binSize: 1.0,
-            fixedBinSize: false
+            fixedBinSize: false,
+            type: "plume"
         },
         windspeed: {
             name: "S10m",
             units: "m/s",
             colours: [d3.interpolatePurples(0.8), d3.interpolatePurples(0.5), d3.interpolatePurples(0.2)],
             binSize: 1.0,
-            fixedBinSize: false
+            fixedBinSize: false,
+            type: "plume"
         },
         winddir: {
             name: "D10m",
             units: "&deg",
             colours: [d3.interpolateOranges(0.8), d3.interpolateOranges(0.5), d3.interpolateOranges(0.2)],
             binSize: 10.0,
-            fixedBinSize: false
+            fixedBinSize: false,
+            type: "plume"
         },
         precip: {
             name: "Pcp",
-            units: "mm"
+            units: "mm",
+            binSize: 1.0,
+            fixedBinSize: true,
+            type: "stackedProb"
         },
         cloud: {
             name: "CAF",
             units: "%",
             colours: [d3.interpolateGreys(0.8), d3.interpolateGreys(0.5), d3.interpolateGreys(0.2)],
             binSize: 10.0,
-            fixedBinSize: true
+            fixedBinSize: true,
+            type: "plume"
         },
         hum: {
             name: "RH2m",
             units: "%",
             colours: [d3.interpolatePuBuGn(0.8), d3.interpolatePuBuGn(0.5), d3.interpolatePuBuGn(0.2)],
             binSize: 5.0,
-            fixedBinSize: false
+            fixedBinSize: false,
+            type: "plume"
         }
     }
     return(params[param])
@@ -52,7 +61,9 @@ const getParamInfo = (param) => {
 // async function to load data
 
 const getData = async () => {
-    const data = await d3.json("./data/meps_major_cities_2022081500_1600.json")
+    let data = await d3.json("./data/meps_major_cities_2022081500_1600.json")
+    // Set any negative precipitation values to 0
+    data = data.map(d => ("Pcp" in d) ? d.Pcp < 0 ? ({...d, Pcp: 0.0}) : d : d)
     return data
 }
 
@@ -60,7 +71,7 @@ const getData = async () => {
 // Set up chart dimensions
 
 const width = 800;
-const height = 200;
+const height = 100;
 const margin = {
     top: 15,
     right: 40,
@@ -68,7 +79,7 @@ const margin = {
     left: 40
 };
 
-// Function to set up the plot
+// Function to set up a plume plot
 const plumeChart = (divID, data, param, thresh) => {
 
     let svg = d3.select("#" + param);
@@ -98,23 +109,58 @@ const plumeChart = (divID, data, param, thresh) => {
         .forceBinSize(currentParam.fixedBinSize)
         .render()
 
-    const cont = document.getElementById(divID);
-    cont.appendChild(svg.node());
+    d3.select("#" + divID).append(() => svg.node())
+ 
 }
+
+// Function to set up a stacked probability plot
+const stackChart = (divID, data, param, thresh) => {
+
+    let svg = d3.select("#" + param);
+
+    if (svg.empty()) {
+        svg = d3.create("svg")
+            .attr("id", param)
+            //.attr("viewBox", [0, 0, width, height])
+            .attr("width", width)
+            .attr("height", height)
+            .attr("font-family", "sans-serif");
+    }
+
+    const currentParam = getParamInfo(param);
+
+    new stackedProb(svg)
+        .data(data)
+        .size(width, height)
+        .paramKey(currentParam.name)
+        .paramUnit(currentParam.units)
+        .margins(margin.top, margin.right, margin.bottom, margin.left)
+        //.colours(...currentParam.colours)
+        .bg("#FFF")
+        .textColour("#999")
+        .threshold(thresh)
+        .binSize(currentParam.binSize)
+        .forceBinSize(currentParam.fixedBinSize)
+        .render()
+
+    d3.select("#" + divID).append(() => svg.node())
+ 
+}
+
 
 // Function to refersh charts and sliders (creates on first call)
 const refreshApp = async (dataIn) => {
+    const params = ["temp", "windspeed", "hum", "winddir", "cloud", "precip"]
     const data = await dataIn;
 
     // station selector
     const dropdown = document.createElement("select");
     dropdown.setAttribute("id", "choose-station")
     dropdown.addEventListener("change", (event) => {
-        drawChart(data, "temp");
-        drawChart(data, "windspeed");
-        drawChart(data, "hum");
-        drawChart(data, "winddir");
-        drawChart(data, "cloud");
+        for (const prm of params) {
+            updateThreshSlider(data, prm);
+            drawChart(data, prm);
+        }
     });
     const stations = [...new Set(data.map(d => d.name))].sort();
     for (let i = 0; i < stations.length; i++) {
@@ -131,25 +177,29 @@ const refreshApp = async (dataIn) => {
     locContainer.appendChild(dropdownLabel);
     locContainer.appendChild(dropdown);
 
-    // Threshold
-    makeThreshSlider(data, "temp");
-    makeThreshSlider(data, "windspeed");
-    makeThreshSlider(data, "hum");
-    makeThreshSlider(data, "winddir");
-    makeThreshSlider(data, "cloud");
+    // Threshold Sliders
+    for (const prm of params) {
+        makeThreshSlider(data, prm);
+    }
 
-    drawChart(data, "temp");
-    drawChart(data, "windspeed");
-    drawChart(data, "hum");
-    drawChart(data, "winddir");
-    drawChart(data, "cloud");
+    // Draw charts for the first time
+    for (const prm of params) {
+        drawChart(data, prm);
+    }
 }
 
 const data = getData();
 
 refreshApp(data);
 
+const filterToLocation = data => {
+    const loc = document.getElementById("choose-station").value
+    console.log(data)
+    return data.filter(d => d.name === loc)
+}
+
 const makeThreshSlider = (data, param) => {
+    //const data = filterToLocation(dataIn);
     const currentParam = getParamInfo(param)
     const threshRange = d3.extent(data, d => d[currentParam.name]);
     const n = currentParam.binSize;
@@ -169,7 +219,8 @@ const makeThreshSlider = (data, param) => {
     threshContainer.appendChild(slider);   
 }
 
-const updateThreshSlider = (data, param) => {
+const updateThreshSlider = (dataIn, param) => {
+    const data = filterToLocation(dataIn);
     const currentParam = getParamInfo(param)
     const threshRange = d3.extent(data, d => d[currentParam.name]);
     const n = currentParam.binSize;
@@ -177,18 +228,22 @@ const updateThreshSlider = (data, param) => {
     slider.setAttribute("min", n * Math.floor(threshRange[0] / n));
     slider.setAttribute("max", n * Math.ceil(threshRange[1] / n));
     slider.setAttribute("value", n * Math.floor(threshRange[0] / n));
-
-    const thresh = slider.value;
-    return thresh;
 }
 
 const drawChart = (dataIn, param) => {
-    // Filter data to station
-    const loc = document.getElementById("choose-station").value;
-    const data = dataIn.filter(d => d.name === loc)
 
-    // Update temperature threshold slider for station
-    const thresh = updateThreshSlider(data, param);
-    plumeChart(param + "Container", data, param, thresh);
+    const data = filterToLocation(dataIn);
+
+    // Get threshold from slider
+    const slider = document.getElementById(param + "-threshold")
+    const thresh = slider.value;
+    const currentParam = getParamInfo(param)
+
+    // Call the chart drawing function
+    currentParam.type === "plume"
+        ? plumeChart(param + "Container", data, param, thresh)
+        : currentParam.type === "stackedProb"
+            ? stackChart(param + "Container", data, param, thresh)
+            : plumeChart(param + "Container", data, param, thresh)
 }
 
---
+++ stackedProb.js
@@ -0,0 +1,471 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import { dotPlot } from "./dotPlot.js"
+
+export class stackedProb {
+    constructor(parent) {
+      this._parent = parent;
+      this._width = 800;
+      this._height = 400;
+      this._margins = {
+        top: 10,
+        right: 30,
+        bottom: 30,
+        left: 30
+      };
+      this._utcTime = false;
+      this._timeFormat = "%Y-%m-%d %H:%M:%S";
+      this._validTimeKey = "valid_dttm";
+      this._data = null;
+      this._paramKey = null;
+      this._paramUnit = "";
+      this._stackColours = {
+        q100q80: d3.interpolateBlues(0.9),
+        q80q60: d3.interpolateBlues(0.7),
+        q60q40: d3.interpolateBlues(0.5),
+        q40q20: d3.interpolateBlues(0.3),
+        q20q0: d3.interpolateBlues(0.1)
+      };
+      this._bg = "#FFF";
+      this._minY = null;
+      this._maxY = null;
+      this._textColour = "#000";
+      this._testVal = null;
+      this._threshold = null;
+      this._binStep = 1;
+      this._forceStep = false;
+    }
+  
+    size(w, h) {
+      this._width = w;
+      this._height = h;
+      return this;
+    }
+  
+    margins(t, r, b, l) {
+      this._margins = { top: t, right: r, bottom: b, left: l };
+      return this;
+    }
+  
+    colours(q100, q80, q60, q40, q20) {
+      this._stackColours = {
+        q100q80: q100,
+        q80q60: q80,
+        q60q40: q60,
+        q40q20: q40,
+        q20q0: q20
+      };
+      return this;
+    }
+  
+    bg(c) {
+      this._bg = c;
+      return this;
+    }
+  
+    textColour(c) {
+      this._textColour = c;
+      return this;
+    }
+  
+    utcTime(utc) {
+      this._utcTime = utc;
+      return this;
+    }
+  
+    timeFormat(t) {
+      this._timeFormat = t;
+      return this;
+    }
+  
+    validTimeKey(k) {
+      this._validTimeKey = k;
+      return this;
+    }
+  
+    data(data) {
+      this._data = data;
+      return this;
+    }
+  
+    threshold(t) {
+      this._threshold = t;
+      return this;
+    }
+  
+    binSize(b) {
+      this._binStep = b;
+      return this;
+    }
+    forceBinSize(f) {
+      this._forceStep = f;
+      return this;
+    }
+  
+    paramKey(k) {
+      this._paramKey = k;
+      return this;
+    }
+  
+    paramUnit(u) {
+      this._paramUnit = u;
+      return this;
+    }
+  
+    ylim(l, h) {
+      const yLims = [l, h].sort();
+      this._minY = yLims[0];
+      this._maxY = yLims[1];
+      return this;
+    }
+  
+    test() {
+      this._testVal = this._toStacks();
+      //this._testVal = this._toDays();
+      return this;
+    }
+  
+    render() {
+      this._drawStack();
+      return this;
+    }
+  
+    //
+  
+    _toQuantiles() {
+      return d3.flatRollup(
+        this._data,
+        (v) => ({
+          q100: d3.min(v.map((d) => d[this._paramKey])),
+          q80: d3.quantile(
+            v.map((d) => d[this._paramKey]),
+            0.2
+          ),
+          q60: d3.quantile(
+            v.map((d) => d[this._paramKey]),
+            0.4
+          ),
+          q40: d3.quantile(
+            v.map((d) => d[this._paramKey]),
+            0.6
+          ),
+          q20: d3.quantile(
+            v.map((d) => d[this._paramKey]),
+            0.8
+          ),
+          q0: d3.max(v.map((d) => d[this._paramKey])),
+          mean: d3.mean(v.map((d) => d[this._paramKey])),
+          control: v.filter((d) => d.member === "mbr000")[0][this._paramKey],
+          prob: d3.mean(
+            v.map((d) => (d[this._paramKey] >= this._threshold ? 1 : 0))
+          )
+        }),
+        (d) => d[this._validTimeKey]
+      );
+    }
+  
+    _toStacks() {
+      const quantileData = this._toQuantiles();
+      return d3
+        .groups(
+          quantileData
+            .map((d) => ({
+              time: d[0],
+              min: d[1].q100,
+              max: d[1].q80,
+              range: "q100q80"
+            }))
+            .concat(
+              quantileData.map((d) => ({
+                time: d[0],
+                min: d[1].q80,
+                max: d[1].q60,
+                range: "q80q60"
+              }))
+            )
+            .concat(
+              quantileData.map((d) => ({
+                time: d[0],
+                min: d[1].q60,
+                max: d[1].q40,
+                range: "q60q40"
+              }))
+            )
+            .concat(
+              quantileData.map((d) => ({
+                time: d[0],
+                min: d[1].q40,
+                max: d[1].q20,
+                range: "q40q20"
+              }))
+            )
+            .concat(
+              quantileData.map((d) => ({
+                time: d[0],
+                min: d[1].q20,
+                max: d[1].q0,
+                range: "q20q0"
+              }))
+            ),
+          (d) => d.range
+        )
+        .map((d) => d[1])
+        .flat();
+    }
+  
+    _barWidths(data, key) {
+      const numBars = [...new Set(data.map((d) => d[key]))].length;
+      return Math.round(
+        ((this._width - this._margins.left - this._margins.right) / numBars) * 0.8
+      );
+    }
+  
+    _toDays() {
+      return [
+        ...new Set(
+          this._data
+            .filter(
+              (d) =>
+                d3.utcFormat("%H")(this._timeParser(d[this._validTimeKey])) ==
+                "00"
+            )
+            .map((d) => d[this._validTimeKey])
+        )
+      ].map((d) => ({
+        data: [
+          { time: this._timeParser(d), y: 0 },
+          {
+            time: this._timeParser(d),
+            y: this._height - this._margins.bottom - this._margins.top
+          }
+        ],
+        text: d3.utcFormat("%a %d %b")(this._timeParser(d))
+      }));
+    }
+  
+    _timeParser(t) {
+      return this._utcTime
+        ? d3.utcParse(this._timeFormat)(t)
+        : d3.timeParse(this._timeFormat)(t);
+    }
+  
+    _drawStack() {
+      this._parent.style("background-color", `${this._bg}`);
+  
+      const selectOrAppend = (elementType, className, parent) => {
+        const selection = parent.select("." + className);
+        if (!selection.empty()) return selection;
+        return parent.append(elementType).attr("class", className);
+      };
+  
+      const x = d3
+        .scaleTime()
+        .domain(
+          d3.extent(
+            this._data.map((d) => this._timeParser(d[this._validTimeKey]))
+          )
+        )
+        .range([0, this._width - this._margins.left - this._margins.right]);
+  
+      const minY = this._minY
+        ? this._minY
+        : d3.min(this._data, (d) => d[this._paramKey]);
+      const maxY = this._maxY
+        ? this._maxY
+        : d3.max(this._data, (d) => d[this._paramKey]);
+  
+      const y = d3
+        .scaleLinear()
+        .domain([minY, maxY])
+        .range([this._height - this._margins.bottom - this._margins.top, 0])
+        .nice();
+  
+      const dayLineGenerator = d3
+        .line()
+        .x((d) => x(d.time))
+        .y((d) => d.y);
+  
+      const bounds = selectOrAppend("g", "bounds", this._parent).attr(
+        "transform",
+        `translate(${this._margins.left},${this._margins.top})`
+      );
+  
+      const rectData = this._toStacks();
+      const barWidth = this._barWidths(rectData, "time");
+      
+      const rects = selectOrAppend("g", "rects", bounds)
+        .selectAll("rect")
+        .data(rectData)
+        .join("rect")
+        .attr("x", d => x(this._timeParser(d.time)) - barWidth / 2)
+        .attr("width", barWidth)
+        .attr("y", d => y(d.max))
+        .attr("height", d => y(d.min) - y(d.max))
+        .attr("fill", (d) => this._stackColours[d.range]);
+  
+      const xAxis = selectOrAppend("g", "xAxis", bounds)
+        .style("font-size", 10)
+        .attr(
+          "transform",
+          `translate(0, ${
+            this._height - this._margins.bottom - this._margins.top
+          })`
+        )
+        .attr("color", `${this._textColour}`)
+        .call(
+          d3
+            .axisBottom(x)
+            .tickFormat(d3.utcFormat("%H"))
+            .ticks(d3.utcHour.every(6))
+        );
+  
+      const yAxis = selectOrAppend("g", "yAxis", bounds)
+        .style("font-size", 10)
+        .attr("color", `${this._textColour}`)
+        .call(d3.axisLeft(y).ticks(5));
+  
+      const dayLines = selectOrAppend("g", "dayLines", bounds)
+        .selectAll("path")
+        .data(this._toDays())
+        .join("path")
+        .attr("fill", "none")
+        .attr("stroke", `${this._textColour}`)
+        .attr("stroke-width", 0.5)
+        .attr("opacity", 0.5)
+        .attr("d", (d) => dayLineGenerator(d.data));
+  
+      const dayText = selectOrAppend("g", "dayText", bounds)
+        .selectAll("text")
+        .data(this._toDays())
+        .join("text")
+        .attr("fill", `${this._textColour}`)
+        .attr("x", (d) => x(d.data[0].time))
+        .attr("y", (d) => 0 - this._margins.top * 0.1)
+        .attr("font-size", 10)
+        .text((d) => d.text);
+  
+      const validTimes = d3.groups(this._data, (d) => d[this._validTimeKey]);
+      const barWidths =
+        x(this._timeParser(validTimes[1][0])) -
+        x(this._timeParser(validTimes[0][0]));
+  
+      const tooltip = selectOrAppend("div", "d3-tooltip", d3.select("body"))
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("opacity", 0)
+        .style("padding", "10px")
+        .style("background", "#FFF")
+        .style("border-radius", "4px")
+        .style("color", "#333")
+        .style("border-style", "solid")
+        .style("border-color", "#C33")
+        .style("font-family", "sans-serif")
+        .text("a simple tooltip");
+  
+      const tooltipReveal = (event, d) => {
+        tooltipBars.attr("opacity", (b) => (b[0] === d[0] ? 0.5 : 0));
+        const xPos = x(d[0]);
+        const dataNow = this._data.filter(
+          (dd) => dd[this._validTimeKey] === d[0]
+        );
+        const totalMembers = dataNow.length;
+        const exceedenceMembers = dataNow.filter(
+          (dd) => dd[this._paramKey] >= this._threshold
+        ).length;
+        tooltip
+          .html(
+            `<div id="stack-tooltip">
+                    <p style="margin: 0; font-size: 0.75em"><u>${d3.utcFormat(
+                      "%H:%M %a %d %b %Y"
+                    )(this._timeParser(d[0]))}</u></p>
+                    <p style="margin: 0; font-size: 0.75em">${exceedenceMembers} of ${totalMembers} members &#8805 ${
+              this._threshold
+            }</p>
+                  </div>`
+          )
+          .style("opacity", 0.8);
+  
+        new dotPlot("stack-tooltip")
+          .data(dataNow)
+          .paramKey(this._paramKey)
+          .threshold(this._threshold)
+          .step(this._binStep)
+          .force(this._forceStep)
+          .colours(this._stackColours.q100q80, this._stackColours.q40q20)
+          .render()
+      };
+  
+      const tooltipHide = () => {
+        tooltipBars.attr("opacity", 0);
+        tooltip.style("opacity", 0).html("");
+      };
+  
+      const tooltipMove = (event, d) => {
+        const xPos =
+          x(this._timeParser(d[0])) > this._width - this._margins.left - 158
+            ? -178
+            : 10;
+        tooltip
+          .style("top", event.pageY - 10 + "px")
+          .style("left", event.pageX + xPos + "px");
+      };
+  
+      const tooltipBars = selectOrAppend("g", "tooltipBars", bounds)
+        .selectAll("rect")
+        .data(this._toQuantiles())
+        .join("rect")
+        .attr("stroke-width", 0)
+        .attr("fill", "#CCC")
+        .attr("opacity", 0)
+        .attr("x", (d) => x(this._timeParser(d[0])) - barWidths / 2)
+        .attr("y", 0)
+        .attr("width", barWidths)
+        .attr("height", this._height - this._margins.top - this._margins.bottom)
+        .on("mouseenter", tooltipReveal)
+        .on("mouseleave", tooltipHide)
+        .on("mousemove", tooltipMove);
+  
+      if (this._threshold) {
+        d3.select("#threshLine").remove();
+        d3.select("#threshText").remove();
+  
+        const yVal =
+          (y(this._threshold) < -1) |
+          (y(this._threshold) >
+            this._height - this._margins.top - this._margins.bottom)
+            ? null
+            : y(this._threshold);
+  
+        const threshData = [
+          { x: 0, y: yVal },
+          {
+            x: this._width - this._margins.left - this._margins.right / 2,
+            y: yVal
+          }
+        ];
+  
+        if (yVal) {
+          const threshLine = selectOrAppend("g", "threshLine", bounds)
+            .append("path")
+            .attr("id", "threshLine")
+            .attr("stroke-width", 1)
+            .attr("stroke", this._textColour)
+            .attr(
+              "d",
+              d3
+                .line()
+                .x((d) => d.x)
+                .y((d) => d.y)(threshData)
+            );
+  
+          const threshText = selectOrAppend("g", "threshText", bounds)
+            .append("text")
+            .attr("id", "threshText")
+            .attr("fill", this._textColour)
+            .attr("x", this._width - this._margins.left - this._margins.right + 2)
+            .attr("y", yVal - 2)
+            .attr("font-size", 10)
+            .html(`${this._threshold}` + this._paramUnit);
+        }
+      }
+    }
+  }
